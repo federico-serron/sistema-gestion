86b0cce0f305b005534acd253340af8b9b9b9cfb
federico-serron <f.serron.umpierrez@gmail.com>
2025-07-27 18:10:25 +0930

    Tabla usuer creada y migraciones hechas

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,32 @@
 from app import db
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, Integer, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+from typing import Optional
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    name: Mapped[str] = mapped_column(String(60), nullable=False)
+    phone: Mapped[str] = mapped_column(String(40), nullable=False)
+    address: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    role: Mapped[str] = mapped_column(String(10), nullable=False, default="user")
+    last_login: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
+    is_premium: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=False)
+
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'name': self.name,
+            'phone': self.phone,
+            'address': self.address,
+            'role': self.role,
+            'last_login': self.last_login,
+            'is_active': self.is_active,
+            'is_premium': self.is_premium,
+        }
